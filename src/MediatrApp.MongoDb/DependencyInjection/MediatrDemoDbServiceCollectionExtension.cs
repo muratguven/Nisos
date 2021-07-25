b86f974b88b3ff2b3f5fd1363db01a6a172f5af1
Murat Güven <muratguven_ktu@hotmail.com>
2021-07-25 21:01:03 +0300

    service collection

---
+++ src/MediatrApp.MongoDb/DependencyInjection/MediatrDemoDbServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatrApp.MongoDb.DependencyInjection
+{
+    public static class MediatrDemoDbServiceCollectionExtension
+    {
+        public static AutofacServiceProvider AddMediatrDemoMongoDb(this IServiceCollection services)
+        {
+            
+
+            ContainerBuilder containerBuilder = new();
+            //containerBuilder.RegisterType<MediatrAppMongoDbContext>().As<IMongoDbContext>().
+            containerBuilder.Populate(services);
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
+           
+        }
+    }
+}
---
+++ src/MediatrApp.MongoDb/Repositories/Customers/CustomerQueryRepository.cs
@@ -0,0 +1,30 @@
+﻿using MediatrApp.Domain.Customers;
+using MediatrApp.MongoDb.Db;
+using MediatrDemo.MongoDb.Db;
+using MediatrDemo.MongoDb.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatrApp.MongoDb.Repositories.Customers
+{
+    public class CustomerQueryRepository : MongoDbQueryRepository<MediatrAppMongoDbContext, Customer>, ICustomerQueryRepository
+    {
+        public CustomerQueryRepository(IMongoDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public Customer Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
